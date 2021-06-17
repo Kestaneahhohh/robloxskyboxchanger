8a3dddc5dd107bac9467a4ba45d14c300c404e68
arda <67337599+Kestaneahhohh@users.noreply.github.com>
2021-06-17 16:53:42 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,11 @@
+//example asset id http://www.roblox.com/asset/?id=6083995041
 
+s = Instance.new("Sky")
+s.Name = "Sky"
+s.SkyboxBk = "id goes here"
+s.SkyboxDn = "id goes here"
+s.SkyboxFt = "id goes here"
+s.SkyboxLf = "id goes here"
+s.SkyboxRt = "id goes here"
+s.SkyboxUp = "id goes here"
+s.Parent = game.Lighting
